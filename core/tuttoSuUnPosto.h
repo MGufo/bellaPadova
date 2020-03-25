4e2c8e846f0e03e548dd60d8c559ed94be2633fa
MGufo <34510538+MGufo@users.noreply.github.com>
2020-03-25 19:48:19 +0100

    struttura classi, inizio scrittura metodi

--- core/tuttoSuUnPosto.h
+++ core/tuttoSuUnPosto.h
@@ -1,57 +1,130 @@
-enum formato { normale, family, mezzo_metro, battuta };
+#include <queue>
+#include <string>
+enum formatoPizza { normale, family, mezzo_metro, battuta };
 enum farina { normale, integrale, kamut, farro };
-enum condimento { normale, bianca, rossa };
+enum formatoBevanda {
+  bottiglia33cl,
+  bottiglia50cl,
+  bottiglia1L,
+  lattina33cl,
+  lattina50cl
+};
+enum tipoFornitore { mercato, supermercato, azienda, altro };
+
+using std::priority_queue;
+using std::string;
 
 class Risorsa {
- public:
-  String nome;
+ private:
+  string nome;
   bool disponibilita;
-  ~Risorsa();
+
+ public:
+  virtual ~Risorsa();
 };
 
-class Articolo : public Risorsa {
- public:
+class Articolo : virtual public Risorsa {
+ private:
   double prezzo;
 };
 
-class Attrezzatura : public Risorsa {
- public:
+class Attrezzatura : virtual public Risorsa {
+ private:
   Fornitore fornitore;
   unsigned int quantita;
   double costo;
-  Data dataAcquisto;
-};
-
-class Pizza : public Articolo {
- public:
-  formato tipoFormato;
-  contenitoreC<string> ingredienti() = 0;
+  // TODO: Import data library
+  // Data dataAcquisto;
 };
 
 class Fritto : public Articolo {
- public:
-  formato tipoFormato;
-  contenitoreC<string> ingredienti() = 0;
+ private:
+  ////contenitoreC <string> ingredienti;
 };
 
-// classe concreta
-class Frittura : public Fritto {
- public:
+class Bevanda : public Articolo, public Attrezzatura {
+ private:
+  formatoBevanda formato;
+};
+
+class Ingrediente : public Attrezzatura {};
+
+class Strumentazione : public Attrezzatura {};
+
+class Pizza : public Articolo {
+ private:
+  formatoPizza tipoFormato;
+  // contenitoreC<string>* ingredienti;
   farina tipoFarina;
-  condimento tipoCondimento;
-  pizzaFarcita(){};
-  contenitoreC<string> ingredienti();
-};
 
-class Bevanda : public Attrezzatura {};
-
-// classe concreta
-class pizzaFarcita : public Pizza {
  public:
-  farina tipoFarina;
-  condimento tipoCondimento;
-  pizzaFarcita(){};
-  contenitoreC<string> ingredienti();
+  // Costruttore di default, costruisce una pizza a impasto normale senza
+  // ingredienti
+  Pizza(formatoPizza fo =
+            formatoPizza::normale,  // contenitoreC<string>* ingr = nullptr,
+        farina fa = farina::normale)
+      : tipoFormato(fo),
+        // ingredienti(ingr ? new //contenitoreC<string>(ingr) : nullptr),
+        tipoFarina(fa){};
+
+  // getter
+  string getTipoFormato() const;
+  // contenitoreC<string> getIngredienti() const;
+  string getTipoFarina() const;
+
+  // setter
+  void setTipoFormato(const unsigned int);
+  void setIngredienti(const string);
+  void setTipoFarina(const unsigned int);
 };
 
-class Fornitore {};
+class Anagrafica {
+ private:
+  string indirizzo;
+  string telefono;
+};
+
+class Cliente {
+ private:
+  string nome;
+  Anagrafica recapito;
+};
+
+class Fornitore {
+ private:
+  string nome;
+  Anagrafica recapito;
+  tipoFornitore tipologia;
+};
+
+class Comanda {
+ private:
+  // TODO: Verificare se si possano usare le mappe STL
+  // contenitoreC<Articolo*> articoli;
+  // contenitoreC<unsigned int> quantita;
+  Cliente cliente;
+  bool omaggio;
+  bool domicilio;
+  // Data data;
+  // Time orario;
+};
+
+class GestioneComande {
+ private:
+  priority_queue<Comanda> coda;
+  // TODO: Metodi per la gestione della coda
+};
+
+class Menu {
+ private:
+  // contenitoreC<Articolo*> articoli;
+};
+
+class Pizzeria {
+ private:
+  string nome;
+  Anagrafica contatto;
+  string pIVA;
+  // contenitoreC<Risorsa*> inventario;
+  Menu* menu;
+};
