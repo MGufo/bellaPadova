9b5aef0fd6969f9036f1f5357444f0e620383b48
MGufo <34510538+MGufo@users.noreply.github.com>
2020-03-30 11:52:11 +0200

    fix riferimento al contenitore in pizza.h, rimossi file obsoleti

--- core/pizza.h
+++ core/pizza.h
@@ -1,25 +1,28 @@
+#ifndef PIZZA.H
+#define PIZZA .H
+
 #include "articolo.h"
+#include "container.h"
 
 class Pizza : public Articolo {
  private:
   formatoPizza tipoFormato;
   farina tipoFarina;
-  contenitoreC<string>* ingredienti;
+  Vettore<string>* ingredienti;
 
  public:
   // Costruttore di default, costruisce una pizza a impasto normale senza
   // ingredienti
   Pizza(string nome = "focaccia", bool disponibilita = true, double prezzo = 0,
-        formatoPizza fo = formatoPizza::normale,
-        farina fa = farina::normale,
-        contenitoreC<string>* ingr = nullptr);
+        formatoPizza fo = formatoPizza::normale, farina fa = farina::normale,
+        Vettore<string>* ingr = nullptr);
   // Costruttore di copia
   Pizza(const Pizza&);
   // Distruttore
   ~Pizza();
   // getter
   formatoPizza getTipoFormato() const;
-  const contenitoreC<string>& getIngredienti() const;
+  const Vettore<string>& getIngredienti() const;
   farina getTipoFarina() const;
 
   // setter
@@ -27,6 +30,7 @@ class Pizza : public Articolo {
   void setTipoFarina(const unsigned int);
   void addIngrediente(const string&);
 
-  //implementazione metodo di clonazione (classe diventa concreta)
+  // implementazione metodo di clonazione (classe diventa concreta)
   Pizza* clone() const;
 };
+#endif
