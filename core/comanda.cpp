c969f02da6924d9c651ea023ed04cc70c8aafb9e
MGufo <34510538+MGufo@users.noreply.github.com>
2020-04-25 19:47:32 +0200

    inizio progettazione metodi classe comanda

--- core/comanda.cpp
+++ core/comanda.cpp
@@ -3,16 +3,38 @@
 /* Gli oggetti passati come parametri al costruttore vengono costruiti ad alto
   livello; per questo motivo non vengono costruiti di copia nel costruttore.
 */
-Comanda::Comanda(unordered_map<Articolo*, unsigned int>* _articoli,
+Comanda::Comanda(unordered_map<Articolo*, unsigned int> _articoli,
                  Contatto* _cliente, QTime _oraConsegna)
     : articoli(_articoli), cliente(_cliente), oraConsegna(_oraConsegna){};
 
 Comanda::Comanda(const Comanda& c)
-    : articoli(new unordered_map<Articolo*, unsigned int>(*c.articoli)),
+    : articoli(c.articoli),
       cliente(new Contatto(*c.cliente)),
       oraConsegna(c.oraConsegna){};
 
 Comanda::~Comanda() {
-  if (articoli) delete articoli;
   if (cliente) delete cliente;
+}
+
+void Comanda::sostituisciArticolo(Articolo* daSostituire, unsigned int qtaDS,
+                                  Articolo* daInserire, unsigned int qtaDI) {
+  if (daSostituire) {
+    if (articoli[daSostituire] <= qtaDS)
+      articoli.erase(daSostituire);
+    else
+      articoli[daSostituire] -= qtaDS;
+  }
+  if (daInserire) articoli[daInserire] = qtaDI;
+}
+// sottocaso 1: aggiungere un nuovo articolo non presente in comanda
+void Comanda::inserisciArticolo(Articolo* _daInserire, unsigned int _qtaDI) {
+  sostituisciArticolo(nullptr, 0, _daInserire, _qtaDI);
+}
+
+// sottocaso 4: rimuovere un articolo presente in comanda
+void Comanda::rimuoviArticolo(Articolo* _daSostituire) {
+  if (&articoli[_daSostituire]) {
+    unsigned int _qtaDS = articoli[_daSostituire];
+    sostituisciArticolo(_daSostituire, _qtaDS, nullptr, 0);
+  }
 }
--- core/comanda.h
+++ core/comanda.h
@@ -6,23 +6,57 @@
 
 #include "articolo.h"
 #include "contatto.h"
+#include "farina.h"
 
 using std::unordered_map;
 
 class Comanda {
  private:
-  std::unordered_map<Articolo*, unsigned int>* articoli;
+  std::unordered_map<Articolo*, unsigned int> articoli;
   Contatto* cliente;
   QTime oraConsegna;
 
  public:
-  Comanda(unordered_map<Articolo*, unsigned int>*, Contatto*, QTime);
+  Comanda(unordered_map<Articolo*, unsigned int>, Contatto*, QTime);
   Comanda(const Comanda&);
   ~Comanda();
-  // aggiunta-rimozione articolo
-  // modifica qta articolo
 
-  //getter setter
+  // .1 aggiungere un nuovo articolo non presente in comanda (se già presente,
+  // incrementa la qta) (param: articoloDaInserire, qta)
+  // caso particolare di .2
+  void inserisciArticolo(Articolo*, unsigned int);
+
+  // .2 sostituire un articolo presente in comanda con un altro
+  // articolo non presente (param: articoloDaSostituire,
+  // qtaArticoloDaSostituire, articoloDaInserire, qtaArticoloDaInserire)
+  void sostituisciArticolo(Articolo*, unsigned int, Articolo*, unsigned int);
+
+  // .3 sostituire un articolo presente in comanda con un altro
+  // articolo non presente (param: articoloDaSostituire,
+  // articoloDaInserire, qtaArticoloDaInserire)
+  // caso particolare di .2
+
+  // .4 rimuovere un articolo presente in comanda (param: articoloDaRimuovere,
+  // qta) caso particolare di .2
+  void rimuoviArticolo(Articolo*);
+
+  // .5 modificare la farina della pizza
+  // (param: articoloDaModificare, Farina* da modificare)
+  void modificaFarina(Articolo*, Farina*);
+  // .6 inserire un nuovo ingrediente nella pizza
+  // (param: articoloDaModificare, Ingrediente* da inserire)
+  // (se già presente non succede nulla e notifica l'utente)
+
+  // .7 rimuovere un nuovo ingrediente nella pizza
+  // (param: articoloDaModificare, Ingrediente* da rimuovere)
+  // (se non presente non succede nulla e notifica l'utente)
+
+  // .8 modifica contatto
+
+  // .9 modifica ora consegna
+  // (delirio)
+
+  // .10 modifica qta articolo presente in comanda
 };
 
 #endif
