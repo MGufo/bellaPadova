53e24b1f919dc5a9716edb34e0a84435fc4b7e2b
MGufo <34510538+MGufo@users.noreply.github.com>
2020-04-26 19:16:31 +0200

    terminata progettazione classe comanda (da testare)

--- core/comanda.cpp
+++ core/comanda.cpp
@@ -3,28 +3,37 @@
 /* Gli oggetti passati come parametri al costruttore vengono costruiti ad alto
   livello; per questo motivo non vengono costruiti di copia nel costruttore.
 */
-Comanda::Comanda(unordered_map<Articolo*, unsigned int> _articoli,
-                 Contatto* _cliente, QTime _oraConsegna)
-    : articoli(_articoli), cliente(_cliente), oraConsegna(_oraConsegna){};
+Comanda::Comanda(unordered_map<Articolo*, unsigned int> _ordinazione,
+                 Contatto _cliente, QTime _oraConsegna)
+    : ordinazione(_ordinazione), cliente(_cliente), oraConsegna(_oraConsegna){};
 
 Comanda::Comanda(const Comanda& c)
-    : articoli(c.articoli),
-      cliente(new Contatto(*c.cliente)),
+    : ordinazione(c.ordinazione),
+      cliente(c.cliente),
       oraConsegna(c.oraConsegna){};
 
-Comanda::~Comanda() {
-  if (cliente) delete cliente;
+const unordered_map<Articolo*, unsigned int>& Comanda::getOrdinazione() const {
+  return ordinazione;
 }
 
-void Comanda::sostituisciArticolo(Articolo* daSostituire, unsigned int qtaDS,
+const Contatto& Comanda::getCliente() const { return cliente; }
+
+const QTime& Comanda::getOraConsegna() const { return oraConsegna; }
+
+void Comanda::setOraConsegna(QTime _oraConsegna) { oraConsegna = _oraConsegna; }
+
+void Comanda::sostituisciArticolo(Articolo* daSostituire, int qtaDS,
                                   Articolo* daInserire, unsigned int qtaDI) {
   if (daSostituire) {
-    if (articoli[daSostituire] <= qtaDS)
-      articoli.erase(daSostituire);
-    else
-      articoli[daSostituire] -= qtaDS;
+    if (qtaDS < 0) {
+      if (ordinazione[daSostituire] <= qtaDS * (-1))
+        ordinazione.erase(daSostituire);
+      else
+        ordinazione[daSostituire] += qtaDS;
+    } else
+      ordinazione[daSostituire] += qtaDS;
   }
-  if (daInserire) articoli[daInserire] = qtaDI;
+  if (daInserire) ordinazione[daInserire] = qtaDI;
 }
 // sottocaso 1: aggiungere un nuovo articolo non presente in comanda
 void Comanda::inserisciArticolo(Articolo* _daInserire, unsigned int _qtaDI) {
@@ -33,8 +42,29 @@ void Comanda::inserisciArticolo(Articolo* _daInserire, unsigned int _qtaDI) {
 
 // sottocaso 4: rimuovere un articolo presente in comanda
 void Comanda::rimuoviArticolo(Articolo* _daSostituire) {
-  if (&articoli[_daSostituire]) {
-    unsigned int _qtaDS = articoli[_daSostituire];
-    sostituisciArticolo(_daSostituire, _qtaDS, nullptr, 0);
-  }
+  ordinazione.erase(_daSostituire);
+}
+
+// controlli su esistenza farina e TD dell'articolo fatti ad alto livello dal
+// chiamante
+void Comanda::modificaFarina(Pizza* _daModificare, Farina* _farina) {
+  _daModificare->setFarina(_farina);
+}
+
+void aggiungiIngrediente(Pizza* _daModificare, Ingrediente* _ingrediente) {
+  _daModificare->addIngrediente(_ingrediente);
+}
+
+void rimuoviIngrediente(Pizza* _daModificare, Ingrediente* _ingrediente) {
+  _daModificare->removeIngrediente(_ingrediente);
+}
+
+void Comanda::modificaContatto(string _nome, string _indirizzo, string _tel) {
+  if (_nome != "") cliente.setNome(_nome);
+  if (_indirizzo != "") cliente.setIndirizzo(_indirizzo);
+  if (_tel != "") cliente.setTelefono(_tel);
+}
+
+void Comanda::modificaQuantita(Articolo* _daModificare, int _qta) {
+  sostituisciArticolo(_daModificare, _qta, nullptr, 0);
 }
--- core/comanda.h
+++ core/comanda.h
@@ -4,32 +4,41 @@
 #include <QTime>
 #include <unordered_map>
 
-#include "articolo.h"
+#include "bevanda.h"
 #include "contatto.h"
 #include "farina.h"
+#include "pizza.h"
 
 using std::unordered_map;
 
 class Comanda {
  private:
-  std::unordered_map<Articolo*, unsigned int> articoli;
-  Contatto* cliente;
+  unordered_map<Articolo*, unsigned int> ordinazione;
+  Contatto cliente;
   QTime oraConsegna;
 
  public:
-  Comanda(unordered_map<Articolo*, unsigned int>, Contatto*, QTime);
-  Comanda(const Comanda&);
-  ~Comanda();
+  Comanda(unordered_map<Articolo*, unsigned int>, Contatto, QTime);
+  Comanda(const Comanda&);  
 
-  // .1 aggiungere un nuovo articolo non presente in comanda (se già presente,
-  // incrementa la qta) (param: articoloDaInserire, qta)
-  // caso particolare di .2
+  const unordered_map<Articolo*, unsigned int>& getOrdinazione() const;
+
+  const Contatto& getCliente() const;
+
+  const QTime& getOraConsegna() const;
+
+  // .9 modifica ora consegna
+  void setOraConsegna(QTime);
+
+  // .1 aggiungere un nuovo articolo non presente in comanda (se già
+  // presente, incrementa la qta) (param: articoloDaInserire, qta) caso
+  // particolare di .2
   void inserisciArticolo(Articolo*, unsigned int);
 
   // .2 sostituire un articolo presente in comanda con un altro
   // articolo non presente (param: articoloDaSostituire,
   // qtaArticoloDaSostituire, articoloDaInserire, qtaArticoloDaInserire)
-  void sostituisciArticolo(Articolo*, unsigned int, Articolo*, unsigned int);
+  void sostituisciArticolo(Articolo*, int, Articolo*, unsigned int);
 
   // .3 sostituire un articolo presente in comanda con un altro
   // articolo non presente (param: articoloDaSostituire,
@@ -42,21 +51,23 @@ class Comanda {
 
   // .5 modificare la farina della pizza
   // (param: articoloDaModificare, Farina* da modificare)
-  void modificaFarina(Articolo*, Farina*);
+  void modificaFarina(Pizza*, Farina*);
+
   // .6 inserire un nuovo ingrediente nella pizza
   // (param: articoloDaModificare, Ingrediente* da inserire)
   // (se già presente non succede nulla e notifica l'utente)
+  void aggiungiIngrediente(Pizza*, Ingrediente*);
 
   // .7 rimuovere un nuovo ingrediente nella pizza
   // (param: articoloDaModificare, Ingrediente* da rimuovere)
   // (se non presente non succede nulla e notifica l'utente)
+  void rimuoviIngrediente(Pizza*, Ingrediente*);
 
   // .8 modifica contatto
-
-  // .9 modifica ora consegna
-  // (delirio)
+  void modificaContatto(string = "", string = "", string = "");
 
   // .10 modifica qta articolo presente in comanda
+  void modificaQuantita(Articolo*, int);
 };
 
 #endif
