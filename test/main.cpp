3c9eeb06b8c6b43115b83ac1873a6f9e89b6d58b
MGufo <34510538+MGufo@users.noreply.github.com>
2020-04-26 19:21:47 +0200

    inizio stesura test per classe 'comanda'

--- test/main.cpp
+++ test/main.cpp
@@ -4,17 +4,24 @@
 #include "../core/ingrediente.h"
 #include "../core/pizza.h"
 #include "../core/qontainer.h"
+#include "../core/comanda.h"
 using std::string;
+
 int main(){
+    Farina* normale = new Farina();
     Ingrediente* mozz = new Ingrediente("mozzarella", true, 10, 5, QDate::currentDate());
+    Ingrediente* pomm = new Ingrediente("pomodoro", true, 10, 5, QDate::currentDate(), true);
     Lista<Ingrediente*>* list = new Lista<Ingrediente*>();
 
-    list->insert(list->begin(),mozz);
+    list->push_back(normale);
+    list->push_back(mozz);
+    list->push_back(pomm);
+    Pizza* marghe = new Pizza("margherita", true, 4, list);
 
-    Pizza* marghe = new Pizza("mozzarella", true, 10, list);
-    delete marghe;
-    delete mozz;
+    unordered_map<Articolo*, unsigned int> ordine;
+    ordine[marghe] = 2;
+    Comanda piergiorgio(ordine, Contatto("AndyM", "Via Prova 123", "123456789"), QTime::currentTime());
+
     std::cout << "ghesboro" << std::endl;
-
     return 0;
 }
