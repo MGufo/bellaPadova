89acbd57f8c1783a1a6cc00048528ed06a09bb82
MGufo <34510538+MGufo@users.noreply.github.com>
2020-04-28 18:33:22 +0200

    classe 'cliente' terminata e testata, close #12

--- core/contatto.cpp
+++ core/contatto.cpp
@@ -3,13 +3,13 @@
 Contatto::Contatto(string _nome, string _indirizzo, string _telefono)
     : nome(_nome), indirizzo(_indirizzo), telefono(_telefono) {}
 
-void Contatto::setNome(const string& n) { nome = n; };
-void Contatto::setIndirizzo(const string& ind) { indirizzo = ind; };
-void Contatto::setTelefono(const string& tel) { telefono = tel; };
+void Contatto::setNome(const string& n) { nome = n; }
+void Contatto::setIndirizzo(const string& ind) { indirizzo = ind; }
+void Contatto::setTelefono(const string& tel) { telefono = tel; }
 
-const string Contatto::getNome() const { return nome; };
-const string Contatto::getIndirizzo() const { return indirizzo; };
-const string Contatto::getTelefono() const { return telefono; };
+const string Contatto::getNome() const { return nome; }
+const string Contatto::getIndirizzo() const { return indirizzo; }
+const string Contatto::getTelefono() const { return telefono; }
 
 const Lista<const string>& Contatto::getContatto() const {
   Lista<const string>* contatti = new Lista<const string>();
@@ -17,4 +17,4 @@ const Lista<const string>& Contatto::getContatto() const {
   contatti->push_back(getIndirizzo());
   contatti->push_back(getTelefono());
   return *contatti;
-};
+}
