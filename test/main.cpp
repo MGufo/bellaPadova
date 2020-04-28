698e5dbd4313f939807d1e9c0c866cb6989d7cb7
MGufo <34510538+MGufo@users.noreply.github.com>
2020-04-28 19:19:30 +0200

    testing classe Comanda

--- test/main.cpp
+++ test/main.cpp
@@ -8,20 +8,32 @@
 using std::string;
 
 int main(){
-    Farina* normale = new Farina();
-    Ingrediente* mozz = new Ingrediente("mozzarella", true, 10, 5, QDate::currentDate());
-    Ingrediente* pomm = new Ingrediente("pomodoro", true, 10, 5, QDate::currentDate(), true);
-    Lista<Ingrediente*>* list = new Lista<Ingrediente*>();
+  Farina* normale = new Farina();
+  Ingrediente* mozz = new Ingrediente("mozzarella", true, 10, 5, QDate::currentDate());
+  Ingrediente* pomm = new Ingrediente("pomodoro", true, 10, 5, QDate::currentDate(), true);
+  Lista<Ingrediente*>* list = new Lista<Ingrediente*>();
 
-    list->push_back(normale);
-    list->push_back(mozz);
-    list->push_back(pomm);
-    Pizza* marghe = new Pizza("margherita", true, 4, list);
+  list->push_back(normale);
+  list->push_back(mozz);
+  list->push_back(pomm);
+  Pizza* marghe = new Pizza("margherita", true, 4, *list);
+  Pizza* mari = new Pizza("marinara", true, 4, *list);
+  Pizza* boschi = new Pizza("boscaiola", true, 4, *list);
 
-    unordered_map<Articolo*, unsigned int> ordine;
-    ordine[marghe] = 2;
-    Comanda piergiorgio(ordine, Contatto("AndyM", "Via Prova 123", "123456789"), QTime::currentTime());
+  unordered_map<Articolo*, unsigned int> ordine;
+  ordine[marghe] = 2;
+  Comanda piergiorgio(ordine, Contatto("AndyM", "Via Prova 123", "123456789"), QTime::currentTime());
 
-    std::cout << "ghesboro" << std::endl;
-    return 0;
+  piergiorgio.inserisciArticolo(mari, 1);
+
+
+  // std::cout << piergiorgio.getOraConsegna().toString().toStdString() <<std::endl;
+  // std::cout << piergiorgio.getCliente().getNome()<<std::endl;
+  // std::cout << piergiorgio.getCliente().getIndirizzo()<<std::endl;
+  // std::cout << piergiorgio.getCliente().getTelefono()<<std::endl;
+
+  std::cout << (piergiorgio.getOrdinazione()).at(mari) << std::endl;
+  std::cout << (piergiorgio.getOrdinazione()).at(boschi) << std::endl;
+  std::cout << (piergiorgio.getOrdinazione()).at(marghe) << std::endl;
+  return 0;
 }
