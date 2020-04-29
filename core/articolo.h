6f19c5824828db938446d151664f56a6524df344
MGufo <34510538+MGufo@users.noreply.github.com>
2020-04-29 16:38:05 +0200

    refactor commenti

--- core/articolo.h
+++ core/articolo.h
@@ -8,10 +8,30 @@ class Articolo : virtual public Risorsa {
   double prezzoBase;
 
  public:
+  /**
+   * @brief: Costruttore di default
+   * @param: string (nome dell'articolo)
+   * @param: bool (disponibilità dell'articolo)
+   * @param: double (costo dell'articolo)
+   */
   Articolo(string, bool, double);
 
+  /**
+   * @brief: Ritorna il prezzo base di un articolo
+   * @return: double prezzoBase
+   */
   double getPrezzoBase() const;
+
+  /**
+   * @brief: Imposta il prezzo base di un articolo
+   * @param: const double& (nuovo prezzo)
+   */
   void setPrezzoBase(const double&);
+
+  /**
+   * @brief: Ritorna il prezzo finale di un articolo
+   * @return: double prezzo
+   */
   virtual double getPrezzo() const = 0;
 };
 #endif
