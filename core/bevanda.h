0bc75bf57ebf62f11842cbd5b40f2fd8a5b7a52a
MGufo <34510538+MGufo@users.noreply.github.com>
2020-04-30 11:20:05 +0200

    refactor commenti

--- core/bevanda.h
+++ core/bevanda.h
@@ -12,14 +12,35 @@ class Bevanda : public Articolo, public Consumabile {
  public:
   Bevanda(string, bool, double, unsigned int, double, QDate, double);
 
-  // getter
+  /**
+   * @brief: Ritorna la spesa sostenuta dalla pizzeria per l'acquisto di una
+   * bevanda.
+   * @return: double (spesa)
+   */
   double getSpesa() const;
+
+  /**
+   * @brief: Ritorna la capacità di una bevanda
+   * @return: double (capacità bevanda in L)
+   */
   double getCapacitaBevanda() const;
+
+  /**
+   * @brief: Ritorna la "plastic tax"
+   * @return: double (costo extra per contenitori in plastica)
+   */
   double getPlasticTax() const;
 
-  // setter
-
+  /**
+   * @brief: Modifica la "plastic tax"
+   * @param: double (costo extra per contenitori in plastica)
+   */
   void setPlasticTax(double);
+
+  /**
+   * @brief: Modifica la capacità della bevanda
+   * @param: double (nuova capacità espressa in L)
+   */
   void setCapacita(double);
 };
 #endif
