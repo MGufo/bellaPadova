4c69c573363235835c3503faac63addc3331bd41
MGufo <34510538+MGufo@users.noreply.github.com>
2020-05-01 18:17:15 +0200

    aggiunta descizione metodo inserisciArticolo

--- core/menu.h
+++ core/menu.h
@@ -12,7 +12,15 @@ class Menu {
 
  public:
   Menu();
+
+  /**
+   * @brief: Inserisce un articolo nel menù. Se già presente non fa nulla.
+   * Se almeno un ingrediente dell'articolo non è disponibile l'articolo sarà
+   * inserito con disponibilità = false.
+   * @param: Articolo* (Articolo da inserire)
+   */
   void inserisciArticolo(Articolo*);
+
   void modificaArticolo(Articolo*);
   void eliminaArticolo(Articolo*);
 };
