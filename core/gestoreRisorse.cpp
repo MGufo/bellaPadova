020222ffc2ccee9b5dd14d7aa2757b13fa9f10aa
MGufo <34510538+MGufo@users.noreply.github.com>
2020-05-08 19:30:44 +0200

    implementati metodi controlloDisponibilita(), inserisciArticoloInMenu(), rimuoviArticoloDaMenu(), inserisciConsumabileInInventario(), rimuoviConsumabileDaInventario(); fornita possibile implementazione arrogante

--- core/gestoreRisorse.cpp
+++ core/gestoreRisorse.cpp
@@ -1,5 +1,100 @@
 #include "gestoreRisorse.h"
 
 GestoreRisorse::GestoreRisorse()
-// TODO: costruire liste nello stack
-    : menu(new Lista<Articolo*>()), inventario(new Lista<Consumabile*>()) {}
+    : menu(Lista<Articolo*>()), inventario(Lista<Consumabile*>()) {}
+
+template <class T>
+T getPosizione(Lista<T*> lista, T* valore) {
+  Lista<T*>::Iterator it;
+  bool trovato = false;
+  for (it = lista.begin(); it != lista.end() && !trovato; ++it)
+    if (*it == valore) trovato = true;
+  return it;
+}
+template <class T>
+T getPosizione(const Lista<T*> lista, T* valore) {
+  Lista<T*>::Iterator it;
+  bool trovato = false;
+  for (it = lista.begin(); it != lista.end() && !trovato; ++it)
+    if (*it == valore) trovato = true;
+  return it;
+}
+
+bool GestoreRisorse::controlloDisponibilita(const Articolo* _articolo) const {
+  Lista<Articolo*>::const_Iterator it;
+  bool trovato = false;
+  for (it = menu.const_begin(); it != menu.const_end() && !trovato; ++it)
+    if (*it == _articolo) trovato = true;
+  return trovato;
+}
+
+bool GestoreRisorse::controlloDisponibilita(
+    const Consumabile* _consumabile) const {
+  Lista<Consumabile*>::const_Iterator it;
+  bool trovato = false;
+  for (it = inventario.const_begin(); it != inventario.const_end() && !trovato;
+       ++it)
+    if (*it == _consumabile) trovato = true;
+  return trovato;
+}
+
+void GestoreRisorse::rimuoviArticoloDaMenu(Articolo* daRimuovere) {
+  Lista<Articolo*>::Iterator it = getPosizione(menu, daRimuovere);
+  // bool trovato = false;
+  if (it != menu.end())
+    // for (it = menu.begin(); it != menu.end() && !trovato; ++it)
+    //   if (*it == daRimuovere) trovato = true;
+    menu.erase(it);
+}
+
+// TODO: Verifica tipo istanziazione container in push_back
+void GestoreRisorse::inserisciConsumabileInInventario(Consumabile* daInserire) {
+  inventario.push_back(daInserire);
+}
+
+void GestoreRisorse::rimuoviConsumabileDaInventario(Consumabile* daRimuovere) {
+  if (!dynamic_cast<Pizza*>(daRimuovere)) {
+    auto it = getPosizione(inventario, daRimuovere);
+    if (it != inventario.end()) inventario.erase(it);
+  }
+  // pizza
+  else {
+    for (auto it = menu.begin(); it != menu.end(); ++it) {
+      if (dynamic_cast<Pizza*>(*it)) {
+        Pizza* temPizza = static_cast<Pizza*>(*it);
+        if (getPosizione(temPizza->getIngredienti(),
+                         static_cast<Ingrediente*>(daRimuovere)) !=
+            temPizza->getIngredienti().end())
+          temPizza->setDisponibilita(false);
+      }
+    }
+  }
+
+  //   if (!dynamic_cast<Farina*>(daRimuovere)) {
+  //     Lista<Articolo*>::Iterator it;
+  //     // pizza
+  //     for (it = menu.begin(); it != menu.end(); ++it) {
+  //       if (dynamic_cast<Pizza*>(*it)) {
+  //         bool trovato = false;
+  //         Pizza* temPizza = static_cast<Pizza*>(*it);
+  //         for (auto it2 = temPizza->getIngredienti().const_begin();
+  //              it2 != temPizza->getIngredienti().const_end(); ++it)
+  //           if (*it2 == daRimuovere) trovato = true;
+  //         if (trovato) temPizza->setDisponibilita(false);
+  //       }
+  //       // bevanda
+  //       else if (dynamic_cast<Bevanda*>(*it)) {
+  //         int x;
+  //       }
+  //       // farina
+  //       else {
+  //         Lista<Consumabile*>::Iterator it;
+  //         bool trovato = false;
+  //         for (it = inventario.begin(); it != inventario.end() && !trovato;
+  //         ++it)
+  //           if (*it == daRimuovere) trovato = true;
+  //         inventario.erase(it);
+  //       }
+  //     }
+  //   }
+  // }
--- core/gestoreRisorse.h
+++ core/gestoreRisorse.h
@@ -1,20 +1,41 @@
 #ifndef GESTORERISORSE_H
 #define GESTORERISORSE_H
-#include "articolo.h"
-#include "consumabile.h"
+#include "bottiglia.h"
+#include "farina.h"
+#include "lattina.h"
+#include "pizza.h"
 #include "qontainer.h"
 
 class GestoreRisorse {
  private:
-  Lista<Articolo*>* menu;
-  Lista<Consumabile*>* inventario;
-
- public:
-  GestoreRisorse();
+  Lista<Articolo*> menu;
+  Lista<Consumabile*> inventario;
 
   // Idea da implementare: la classe gestore deve gestire le liste di Articoli
   // (menu) e consumabili (inventario), permettendo inserimento, modifica,
   // rimozione e ricerca all'interno delle liste
+
+  template <class T>
+  typename Lista<T*>::Iterator getPosizione(Lista<T*> lista, T* valore);
+  template <class T>
+  typename Lista<T*>::Iterator getPosizione(const Lista<T*> lista, T* valore);
+
+ protected:
+  bool controlloDisponibilita(const Articolo*) const;
+  bool controlloDisponibilita(const Consumabile*) const;
+
+ public:
+  GestoreRisorse();
+
+  void inserisciArticoloInMenu();
+  void rimuoviArticoloDaMenu(Articolo*);
+
+  void inserisciConsumabileInInventario(Consumabile*);
+
+  // Nell'eliminare un consumabile si setta la disponibilità delle pizze che
+  // usano quel consumabile a false.
+  // Eccezione: la farina di default NON PUÒ essere eliminata!
+  void rimuoviConsumabileDaInventario(Consumabile*);
 };
 
 #endif
