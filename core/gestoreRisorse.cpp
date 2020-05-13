dfdbec4f642b169cb8f402c065de8eae1d64d206
MGufo <34510538+MGufo@users.noreply.github.com>
2020-05-13 19:44:36 +0200

    reimplementati metodi di inserimento e rimozione per renderli estendibili rispetto la gerarchia di tipi

--- core/gestoreRisorse.cpp
+++ core/gestoreRisorse.cpp
@@ -3,29 +3,49 @@
 GestoreRisorse::GestoreRisorse()
     : menu(Lista<Articolo*>()), inventario(Lista<Consumabile*>()) {}
 
-bool GestoreRisorse::controlloDisponibilita(const Articolo* _articolo) const {
-  Lista<Articolo*>::const_Iterator it;
+bool GestoreRisorse::controlloDisponibilita(
+    const Lista<const Consumabile*>* daCercare) const {
+  Lista<const Consumabile*>::const_Iterator it;
+  bool disponibile = true;
+  for (it = daCercare->const_begin();
+       it != daCercare->const_end() && disponibile; ++it)
+    if (!(*it)->getDisponibilita()) disponibile = false;
+  return disponibile;
+}
+
+bool GestoreRisorse::controlloConsumabile(
+    const Lista<const Consumabile*>* _lista,
+    const Consumabile* daCercare) const {
+  Lista<const Consumabile*>::const_Iterator it;
   bool trovato = false;
-  for (it = menu.const_begin(); it != menu.const_end() && !trovato; ++it)
-    if (*it == _articolo) trovato = true;
+  for (it = _lista->const_begin(); it != _lista->const_end() && !trovato; ++it)
+    if (*it == daCercare) trovato = true;
   return trovato;
+}
+
+/* bool GestoreRisorse::controlloDisponibilita(const Articolo* _articolo)
+const { Lista<Articolo*>::const_Iterator it; bool trovato = false; for (it =
+menu.const_begin(); it != menu.const_end() && !trovato; ++it) if (*it ==
+_articolo) trovato = true; return trovato;
 }
 
 bool GestoreRisorse::controlloDisponibilita(
     const Consumabile* _consumabile) const {
   Lista<Consumabile*>::const_Iterator it;
   bool trovato = false;
-  for (it = inventario.const_begin(); it != inventario.const_end() && !trovato;
+  for (it = inventario.const_begin(); it != inventario.const_end() &&
+!trovato;
        ++it)
     if (*it == _consumabile) trovato = true;
   return trovato;
-}
+} */
 
 void GestoreRisorse::inserisciArticoloInMenu(Articolo* daInserire) {
   if (daInserire) {
-    if (dynamic_cast<Bevanda*>(daInserire))
-      inventario.push_back(dynamic_cast<Consumabile*>(daInserire));
+    const Lista<const Consumabile*>* lista = daInserire->getComposizione();
+    daInserire->setDisponibilita(controlloDisponibilita(lista));
     menu.push_back(daInserire);
+    delete lista;
   }
 }
 
@@ -33,9 +53,6 @@ void GestoreRisorse::rimuoviArticoloDaMenu(Articolo* daRimuovere) {
   if (daRimuovere) {
     Lista<Articolo*>::Iterator it = getPosizione(menu, daRimuovere);
     if (it != menu.end()) {
-      if (dynamic_cast<Bevanda*>(*it))
-        inventario.erase(
-            getPosizione(inventario, dynamic_cast<Consumabile*>(daRimuovere)));
       menu.erase(it);
     }
   }
@@ -44,35 +61,21 @@ void GestoreRisorse::rimuoviArticoloDaMenu(Articolo* daRimuovere) {
 // TODO: Verifica tipo istanziazione container in push_back
 void GestoreRisorse::inserisciConsumabileInInventario(Consumabile* daInserire) {
   if (daInserire) {
-    if (dynamic_cast<Bevanda*>(daInserire))
-      menu.push_back(dynamic_cast<Articolo*>(daInserire));
     inventario.push_back(daInserire);
   }
 }
 
+// TODO: Controllare possibilità di far tornare disponibile una pizza che ha un
+// ingredienti non più disponibili
 void GestoreRisorse::rimuoviConsumabileDaInventario(Consumabile* daRimuovere) {
   if (daRimuovere) {
-    // bevanda
-    if (dynamic_cast<Bevanda*>(daRimuovere)) {
-      Lista<Articolo*>::Iterator it =
-          getPosizione(menu, dynamic_cast<Articolo*>(daRimuovere));
-      if (it != menu.end()) menu.erase(it);
-    } else {
-      // ingrediente
-      for (auto it = menu.begin(); it != menu.end(); ++it) {
-        Pizza* currentPizza = dynamic_cast<Pizza*>(*it);
-        if (currentPizza) {
-          if (currentPizza->checkIngrediente(
-                  static_cast<Ingrediente*>(daRimuovere)))
-            currentPizza->setDisponibilita(false);
-        }
-      }
+    for (auto it = menu.begin(); it != menu.end(); ++it) {
+      const Lista<const Consumabile*>* lista = (*it)->getComposizione();
+      if (controlloConsumabile(lista, daRimuovere)) it = menu.erase(it);
+      if (it != menu.begin()) --it;
+      delete lista;
     }
-    // farina di default
-    Farina* tmp = dynamic_cast<Farina*>(daRimuovere);
-    if (!tmp || (tmp && tmp->getTipoFarina() != "tipo 00")) {
-      inventario.erase(getPosizione(inventario, daRimuovere));
-    }
+    inventario.erase(getPosizione(inventario, daRimuovere));
   }
 }
 
