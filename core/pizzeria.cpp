2da0992c76c2361876c6d799b8b930c3f1dbdad8
MGufo <34510538+MGufo@users.noreply.github.com>
2020-05-17 18:17:36 +0200

    implementate operazioni fornite dal modello

--- core/pizzeria.cpp
+++ core/pizzeria.cpp
@@ -7,14 +7,62 @@ Pizzeria::Pizzeria()
       gestoreRisorse(GestoreRisorse()),
       gestoreComande(GestoreComande()) {}
 
-const Lista<const string> Pizzeria::getContatto() const {
+void Pizzeria::inserisciArticolo(Articolo* daInserire) {
+  gestoreRisorse.inserisciArticolo(daInserire);
+}
+
+void Pizzeria::modificaArticolo(Articolo* daModificare,
+                                const Articolo* modificato) {
+  gestoreRisorse.modificaArticolo(daModificare, modificato);
+  delete modificato;
+}
+
+void Pizzeria::rimuoviArticolo(Articolo* daRimuovere) {
+  gestoreRisorse.rimuoviArticolo(daRimuovere);
+  delete daRimuovere;
+}
+
+void Pizzeria::inserisciConsumabile(Consumabile* daInserire) {
+  gestoreRisorse.inserisciConsumabile(daInserire);
+}
+
+void Pizzeria::modificaConsumabile(Consumabile* daModificare,
+                                   const Consumabile* modificato) {
+  gestoreRisorse.modificaConsumabile(daModificare, modificato);
+  delete modificato;
+}
+
+void Pizzeria::rimuoviConsumabile(Consumabile* daRimuovere) {
+  gestoreRisorse.rimuoviConsumabile(daRimuovere);
+  delete daRimuovere;
+}
+
+void Pizzeria::inserisciComanda(Comanda* daInserire) {
+  gestoreComande.inserisciComanda(daInserire);
+}
+
+void Pizzeria::modificaComanda(Comanda* daModificare,
+                               const Comanda* modificata) {
+  gestoreComande.modificaComanda(daModificare, modificata);
+  delete modificata;
+}
+
+void Pizzeria::eseguiComanda() { gestoreComande.eseguiComanda(); }
+
+void Pizzeria::rimuoviComanda(Comanda* daRimuovere) {
+  gestoreComande.rimuoviComanda(daRimuovere);
+  delete daRimuovere;
+}
+
+const Lista<string>& Pizzeria::getContatto() const {
   return contatto.getContatto();
 }
 
-unsigned short Pizzeria::getCapacitaForno() {
-  return capacitaForno;
+void Pizzeria::setContatto(const Contatto* modificato) {
+  contatto = *modificato;
 }
 
+unsigned short Pizzeria::getCapacitaForno() { return capacitaForno; }
 
 void Pizzeria::setCapacitaForno(unsigned short _forno) {
   capacitaForno = _forno;
--- core/pizzeria.h
+++ core/pizzeria.h
@@ -1,6 +1,7 @@
 #ifndef PIZZERIA_H
 #define PIZZERIA_H
 #include <QDate>
+#include <stdexcept>
 
 #include "contatto.h"
 #include "gestoreComande.h"
@@ -13,33 +14,53 @@ class Pizzeria {
   GestoreComande gestoreComande;
   static unsigned short capacitaForno;
 
+  // TODO: Creare gerarchia di eccezioni appropriata ai possibili tipi di errore
+  // e implementarla
+
+  // TODO: Sostituire le 'throw' generiche presenti nel modello con i tipi di
+  // eccezione creati
+  
  public:
   Pizzeria();
   double contabilizzazione(QDate, QDate) const;
-  const Lista<const string> getContatto() const;
 
   // funzione che inserisce un articolo in menu, l'articolo da inserire viene
   // costruito da un'altra funzione che richiama inserisciArticoloInMenu. se
   // tutti gli ingredienti dell'articolo sono disponibili e presenti
   // nell'inventario, la disponibilita dell'articolo viene messa a true;
   // altrimenti false
-  void inserisciArticoloInMenu(Articolo*);
+  void inserisciArticolo(Articolo*);
+  void modificaArticolo(Articolo*, const Articolo*);
+  void rimuoviArticolo(Articolo*);
 
-  // 1. creazione nuova comanda (gestoreRisorse controlla se sia possibile
-  // creare la comanda)
-  // 2. si invoca il metodo di gestoreComande che inserirà la comanda creata
-  // nella lista per gestire le comande
-  void nuovaComanda(Comanda*);
+  void inserisciConsumabile(Consumabile*);
+  void modificaConsumabile(Consumabile*, const Consumabile*);
+  void rimuoviConsumabile(Consumabile*);
 
-  // gestoreComande.lista.*iteratore.
-  // void modificaComanda(Comanda*);
+  void inserisciComanda(Comanda*);
+  void modificaComanda(Comanda*, const Comanda*);
+  void eseguiComanda();
+  void rimuoviComanda(Comanda*);
+
+  const Lista<string>& getContatto() const;
+
+  void setContatto(const Contatto*);
 
   static unsigned short getCapacitaForno();
-
   /**
    * @brief: Modifica la capacità del forno
    * @param: unsigned int (capacità)
    */
   static void setCapacitaForno(unsigned short);
+
+  /**
+   * @brief: Lettura da file
+   */
+  void salva();
+
+  /**
+   * @brief: Scrittura su file
+   */
+  void carica();
 };
 #endif
