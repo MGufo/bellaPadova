9b4551faf7ab8642a0a983450e15aac728277fe5
MGufo <34510538+MGufo@users.noreply.github.com>
2020-05-20 13:20:58 +0200

    bozza implementazione eccezioni

--- core/contatto.cpp
+++ core/contatto.cpp
@@ -1,19 +1,19 @@
 #include "contatto.h"
 
 Contatto::Contatto(string _nome, string _indirizzo, string _telefono)
-    : nome(_nome), indirizzo(_indirizzo), telefono(_telefono) {
-  if (nome.empty()) throw;
-}
+    : nome(_nome), indirizzo(_indirizzo), telefono(_telefono) {}
 
 Contatto::Contatto(const Contatto& c)
     : nome(c.nome), indirizzo(c.indirizzo), telefono(c.telefono) {
-  if (nome.empty()) throw;
+  if (indirizzo.empty())
+    throw new std::invalid_argument("Error: indirizzo inserito non valido.");
 }
 
 // TODO: Add exception type
 void Contatto::setNome(const string& n) { nome = n; }
 void Contatto::setIndirizzo(const string& ind) {
-  if (indirizzo.empty()) throw;
+  if (ind.empty())
+    throw new std::invalid_argument("Error: indirizzo inserito non valido.");
   indirizzo = ind;
 }
 void Contatto::setTelefono(const string& tel) { telefono = tel; }
