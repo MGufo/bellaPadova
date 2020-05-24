c6a8abdd8361cf9f9440b91a7e0e4095928f8b98
MGufo <34510538+MGufo@users.noreply.github.com>
2020-05-24 15:49:34 +0200

    aggiunta classe MainWindow

---
+++ GUI/MainWindow.h
@@ -0,0 +1,13 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QWidget>
+#include <QVBoxLayout>
+
+class MainWindow : public QWidget {
+ Q_OBJECT
+
+private:
+  QVBoxLayout *mainLayout;
+}
+#endif
