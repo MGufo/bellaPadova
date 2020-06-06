40cf806ee2ffee6cc8c0c3366f4b34e884e69176
MGufo <34510538+MGufo@users.noreply.github.com>
2020-06-06 20:31:39 +0200

    modificati nomi enum pagine

--- GUI/wizardNuovoArticolo/wizardNuovoArticolo.h
+++ GUI/wizardNuovoArticolo/wizardNuovoArticolo.h
@@ -3,17 +3,22 @@
 
 #include <QWidget>
 #include <QWizard>
+#include <QMessageBox>
+#include <QString>
+
+#include "GUI/wizardNuovoArticolo/wizard_intro.h"
+#include "GUI/wizardNuovoArticolo/wizard_Pizza.h"
+#include "GUI/wizard_Bevanda"
+#include "GUI/wizard_End"
 
 class WizardNuovoArticolo : public QWizard{
   Q_OBJECT
 public:
+  enum { PAGE_Intro, PAGE_Pizza, PAGE_Bevanda, PAGE_End};
   explicit WizardNuovoArticolo(QWidget *parent = nullptr);
-  enum { Page_tipoArticolo, Page_tipoPizza, Page_tipoBevanda, Page_end};
 
-signals:
-
-public slots:
-  //void accept() override;
+private slots:
+  void mostraAiuto();
 };
 
 #endif // WIZARDNUOVOARTICOLO_H
