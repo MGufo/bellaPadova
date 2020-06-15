6c1fd51c3f5cdc32ab184cc42ce5cbc58e8b9d65
MGufo <34510538+MGufo@users.noreply.github.com>
2020-06-15 16:26:32 +0200

    refactor codice

--- core/pizzeria.cpp
+++ core/pizzeria.cpp
@@ -1,32 +1,32 @@
 #include "pizzeria.h"
 
 Pizzeria::Pizzeria()
-    : contatto(Contatto()),
-      gestoreRisorse(GestoreRisorse()),
-      gestoreComande(GestoreComande()),
-      capacitaForno(5) {}
+  : contatto(Contatto()),
+    gestoreRisorse(GestoreRisorse()),
+    gestoreComande(GestoreComande()),
+    capacitaForno(5) {}
 
 double Pizzeria::contabilizzazione(const QDate & inizio, const QDate & fine) const{
-    double guadagni = 0;
-    double costi = 0;
-    const unordered_map<Articolo*, unsigned int>* ordinazione = nullptr;
-    const Lista<Comanda*>* comande = &gestoreComande.getBacheca();
-    const Lista<Consumabile*>* consumabili = &gestoreRisorse.getInventario();
+  double guadagni = 0;
+  double costi = 0;
+  const unordered_map<Articolo*, unsigned int>* ordinazione = nullptr;
+  const Lista<Comanda*>* comande = &gestoreComande.getBacheca();
+  const Lista<Consumabile*>* consumabili = &gestoreRisorse.getInventario();
 
-    for(auto it = comande->const_begin(); it != comande->const_end(); ++it){
-        if(inizio <= (*it)->getDataConsegna() && (*it)->getDataConsegna() <= fine){
-            ordinazione = &(*it)->getOrdinazione();
-            for(auto it2 =ordinazione->cbegin(); it2 != ordinazione->cend(); ++it2){
-                guadagni += (*it2).first->getPrezzo() * (*it2).second;
-            }
-        }
+  for(auto it = comande->const_begin(); it != comande->const_end(); ++it){
+    if(inizio <= (*it)->getDataConsegna() && (*it)->getDataConsegna() <= fine){
+      ordinazione = &(*it)->getOrdinazione();
+      for(auto it2 =ordinazione->cbegin(); it2 != ordinazione->cend(); ++it2){
+        guadagni += (*it2).first->getPrezzo() * (*it2).second;
+      }
     }
-    for(auto it = consumabili->const_begin(); it != consumabili->const_end(); ++it){
-        if((inizio <= (*it)->getDataAcquisto()) && ((*it)->getDataAcquisto() <= fine)){
-            costi += (*it)->getSpesa();
-        }
+  }
+  for(auto it = consumabili->const_begin(); it != consumabili->const_end(); ++it){
+    if((inizio <= (*it)->getDataAcquisto()) && ((*it)->getDataAcquisto() <= fine)){
+      costi += (*it)->getSpesa();
     }
-    return (guadagni - costi);
+  }
+  return (guadagni - costi);
 }
 
 void Pizzeria::inserisciArticolo(Articolo* daInserire) {
@@ -61,28 +61,28 @@ void Pizzeria::rimuoviConsumabile(Consumabile* daRimuovere) {
 
 void Pizzeria::inserisciComanda(Comanda* daInserire) {
   if(daInserire){
-      //controllo che per ogni elemento dell'ordinazione tutti gli elementi
-      //della sua composizione siano presenti nell'inventario
-      bool inseribile = true;
-      const unordered_map<Articolo*, unsigned int>* ordinazione = &daInserire->getOrdinazione();
-      for(auto it = ordinazione->cbegin(); (it != ordinazione->cend()) && inseribile; ++it){
-          inseribile = gestoreRisorse.controlloInInventario((*it).first);
-      }
-      if(inseribile) gestoreComande.inserisciComanda(daInserire, capacitaForno);
-      else  throw;
+    //controllo che per ogni elemento dell'ordinazione tutti gli elementi
+    //della sua composizione siano presenti nell'inventario
+    bool inseribile = true;
+    const unordered_map<Articolo*, unsigned int>* ordinazione = &daInserire->getOrdinazione();
+    for(auto it = ordinazione->cbegin(); (it != ordinazione->cend()) && inseribile; ++it){
+      inseribile = gestoreRisorse.controlloInInventario((*it).first);
+    }
+    if(inseribile) gestoreComande.inserisciComanda(daInserire, capacitaForno);
+    else  throw;
   }
 }
 
 void Pizzeria::modificaComanda(Comanda* daModificare,
                                Comanda* modificata) {
-    bool inseribile = true;
-    const unordered_map<Articolo*, unsigned int>* ordinazione = &modificata->getOrdinazione();
-    for(auto it = ordinazione->cbegin(); (it != ordinazione->cend()) && inseribile; ++it){
-        inseribile = gestoreRisorse.controlloInInventario((*it).first);
-    }
-    if(inseribile) gestoreComande.modificaComanda(daModificare, modificata, capacitaForno);
-    else throw;
-    delete modificata;
+  bool inseribile = true;
+  const unordered_map<Articolo*, unsigned int>* ordinazione = &modificata->getOrdinazione();
+  for(auto it = ordinazione->cbegin(); (it != ordinazione->cend()) && inseribile; ++it){
+    inseribile = gestoreRisorse.controlloInInventario((*it).first);
+  }
+  if(inseribile) gestoreComande.modificaComanda(daModificare, modificata, capacitaForno);
+  else throw;
+  delete modificata;
 }
 
 void Pizzeria::eseguiComanda() { gestoreComande.eseguiComanda(); }
