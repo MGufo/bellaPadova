3a607dbcfb338ede8f435166bccaf2e1e673c2a0
AndreaMoscon <moscalan96@gmail.com>
2020-06-17 19:02:48 +0200

    avanzamento css con logo

--- GUI/header.cpp
+++ GUI/header.cpp
@@ -3,23 +3,31 @@
 
 Header::Header(QWidget *parent) : QWidget(parent){
   layoutHeader = new QHBoxLayout(this);
-  QLabel* bellaPadova = new QLabel("Pizzeria Bella Padova",this);
+  QPixmap image(":/resources/Bella Padova.png");
+  QLabel* bellaPadova = new QLabel("", this);
+  bellaPadova->setObjectName("bellaPadova");
+  bellaPadova->setMinimumWidth(200);
+  bellaPadova->setMinimumHeight(200);
+  bellaPadova->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+  int w = bellaPadova->width();
+  int h = bellaPadova->height();
+  bellaPadova->setPixmap(image.scaled(w,h,Qt::KeepAspectRatio));
   QLabel* indirizzo = new QLabel("Via Gioacchino Rossini, 20",this);
   QLabel* telefono = new QLabel("1234567890", this);
-  layoutHeader->addWidget(bellaPadova);
+  layoutHeader->addWidget(bellaPadova, 0, Qt::AlignLeft);
   layoutHeader->addWidget(indirizzo);
   layoutHeader->addWidget(telefono);
   //analogClock* orologio = new analogClock(this);
   DigitalClock* orologio = new DigitalClock(this);
-  layoutHeader->addWidget(orologio);
+  orologio->setObjectName("orologio");
+  layoutHeader->addWidget(orologio, 0, Qt::AlignCenter);
   setStyleHeader();
   setLayout(layoutHeader);
 }
 
 void Header::setStyleHeader(){
-  layoutHeader->setSpacing(0);
-  layoutHeader->setMargin(0);
-  setMinimumSize(QSize(900,100));
-  setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
-  //layoutHeader->setContentsMargins(-1,-1,-1,0);
+  //layoutHeader->setSpacing(0);
+  setMinimumSize(QSize(900,200));
+  setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+  //setContentsMargins(0,0,0,0);
 }
