bab1ba7141e3c487c68f4ed5a3369ee4f62f1208
AndreaMoscon <moscalan96@gmail.com>
2020-06-20 18:01:46 +0200

    sistemati alcuni nomi di oggetti

--- GUI/comandagui.cpp
+++ GUI/comandagui.cpp
@@ -3,13 +3,17 @@
 ComandaGUI::ComandaGUI(QWidget *parent) : QWidget(parent){
     layoutComanda = new QVBoxLayout(this);
     QPushButton* wrapperComanda = new QPushButton(this);
+    wrapperComanda->setObjectName("wrapperComanda");
     QVBoxLayout* layoutWrapper = new QVBoxLayout(wrapperComanda);
-
     QHBoxLayout* layoutButtons = new QHBoxLayout();
     QLabel* orario = new QLabel("Orario", wrapperComanda);
+    orario->setObjectName("Orario");
     QLabel* comandaId = new QLabel("Comanda #1", wrapperComanda);
+    comandaId->setObjectName("ComandaId");
     QPushButton* rimuovi = new QPushButton("Rimuovi", wrapperComanda);
+    rimuovi->setObjectName("Rimuovi");
     QPushButton* completata = new QPushButton("Completata", wrapperComanda);
+    completata->setObjectName("Completata");
     layoutButtons->addWidget(rimuovi);
     layoutButtons->addWidget(completata);
 
