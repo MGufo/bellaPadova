e55e20ac59d094666427f28b888d221a065d6bfc
MGufo <34510538+MGufo@users.noreply.github.com>
2020-07-16 17:04:30 +0200

    fix #define error

--- GUI/pacchetti.h
+++ GUI/pacchetti.h
@@ -4,39 +4,40 @@
 #include <string>
 #include <QDate>
 
-#define u_int unsigned int
-
 using std::string;
 
 struct pacchetto_ingrediente{
-  u_int id;
+  uint id;
   string nome;
   bool disponibilita;
-  u_int quantita;
+  uint quantita;
   double costo;
   QDate dataAcquisto;
   bool locale;
   pacchetto_ingrediente* next;
 
-  pacchetto_ingrediente(u_int _ID, string _n, bool _d, u_int _q, double _c,
-                        QDate _da, bool _l, pacchetto_ingrediente* _p = nullptr) :
+  pacchetto_ingrediente(uint _ID, string _n, bool _d, uint _q,
+                        double _c, QDate _da, bool _l,
+                        pacchetto_ingrediente* _p = nullptr) :
     id(_ID), nome(_n), disponibilita(_d), quantita(_q), costo(_c),
     dataAcquisto(_da), locale(_l), next(_p) {}
 };
 
 struct pacchetto_bevanda{
-  u_int id;
+  uint id;
   string nome;
   bool disponibilita;
-  u_int quantita;
+  uint quantita;
   double costo;
   QDate dataAcquisto;
   bool locale;
   pacchetto_bevanda* next;
 
-  pacchetto_bevanda(u_int _ID, string _n, bool _d, u_int _q, double _c,
-                        QDate _da, bool _l, pacchetto_bevanda* _p = nullptr) :
+  pacchetto_bevanda(uint _ID, string _n, bool _d, uint _q,
+                    double _c, QDate _da, bool _l,
+                    pacchetto_bevanda* _p = nullptr) :
     id(_ID), nome(_n), disponibilita(_d), quantita(_q), costo(_c),
     dataAcquisto(_da), locale(_l), next(_p) {}
 };
+
 #endif // PACCHETTI_H
