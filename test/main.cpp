f85316ba4e781da5fdf1c3ff359896d2350bff24
MGufo <34510538+MGufo@users.noreply.github.com>
2020-08-14 16:17:05 +0200

    comande temporaneamente decommentate per testare le funzioni di aggiunta

--- test/main.cpp
+++ test/main.cpp
@@ -19,87 +19,80 @@ int main(int argc, char* argv[]) {
   view.show();
   controller.caricaRisorse();
 
-    /*
-    //string _nome, bool _disponibilita, unsigned int _quantita,
-    //double _costo, QDate _dataAcquisto, bool _locale, string _tipoFarina
-    Farina* normale = new Farina(1);
-    Farina* farro = new Farina(2, "farina di tipo farro 1kg", true, 10, 2, QDate::currentDate(), false, "farro");
+  /*
+  //string _nome, bool _disponibilita, unsigned int _quantita,
+  //double _costo, QDate _dataAcquisto, bool _locale, string _tipoFarina
+  Farina* normale = new Farina(1);
+  Farina* farro =
+      new Farina(2, "farina di tipo farro 1kg", true, 10, 2,
+                 QDate::currentDate(), false, "farro");
 
-    //string _nome, bool _disponibilita, unsigned int _quantita,
-    //double _costo, QDate _dataAcquisto, bool _locale
-    Ingrediente* mozz = new Ingrediente(3, "mozzarella", true, 10, 5, QDate::currentDate());
-    Ingrediente* pomm = new Ingrediente(4, "pomodoro", true, 10, 5, QDate::currentDate(), true);
-    Ingrediente* wrustel = new Ingrediente(5, "wrustel", true, 10, 5, QDate::currentDate());
-    Ingrediente* funghi = new Ingrediente(6, "funghi", true, 10, 5, QDate::currentDate(), true);
-    Ingrediente* ananas = new Ingrediente(7, "ananas", true, 10, 5, QDate::currentDate(), true);
+  //string _nome, bool _disponibilita, unsigned int _quantita,
+  //double _costo, QDate _dataAcquisto, bool _locale
+  Ingrediente* mozz =
+      new Ingrediente(3, "mozzarella", true, 10, 5, QDate::currentDate());
+  Ingrediente* pomm =
+      new Ingrediente(4, "pomodoro", true, 10, 5, QDate::currentDate(), true);
+  Ingrediente* wrustel =
+      new Ingrediente(5, "wrustel", true, 10, 5, QDate::currentDate());
+  Ingrediente* funghi =
+      new Ingrediente(6, "funghi", true, 10, 5, QDate::currentDate(), true);
+  Ingrediente* ananas =
+      new Ingrediente(7, "ananas", true, 10, 5, QDate::currentDate(), true);
 
-    Lista<Ingrediente*>* iniziale = new Lista<Ingrediente*>();
-    Lista<Ingrediente*>* daAggiungere= new Lista<Ingrediente*>();
-    Lista<Ingrediente*>* daRimuovere= new Lista<Ingrediente*>();
+  Lista<Ingrediente*>* iniziale = new Lista<Ingrediente*>();
+  Lista<Ingrediente*>* daAggiungere= new Lista<Ingrediente*>();
+  Lista<Ingrediente*>* daRimuovere= new Lista<Ingrediente*>();
 
-    iniziale->push_back(mozz);
-    iniziale->push_back(pomm);
+  iniziale->push_back(mozz);
+  iniziale->push_back(pomm);
 
-    daAggiungere->push_back(wrustel);
-    daAggiungere->push_back(ananas);
+  daAggiungere->push_back(wrustel);
+  daAggiungere->push_back(ananas);
 
-    daRimuovere->push_back(funghi);
+  daRimuovere->push_back(funghi);
 
-    //string nome, bool disponibilita, double prezzo
-    Pizza* marghe = new Pizza(8, "margherita", true, 4);
-    try{
-      marghe->aggiungiIngredienti(*iniziale);
-    }catch(...){
-      std::cout << "Hai sbagliato, ebete!" << std::endl;
-    }
-    marghe->setFarina(normale);
+  //string nome, bool disponibilita, double prezzo
+  Pizza* marghe = new Pizza(8, "margherita", true, 4);
+  try{
+    marghe->aggiungiIngredienti(*iniziale);
+  }catch(...){
+    std::cout << "Hai sbagliato, ebete!" << std::endl;
+  }
+  marghe->setFarina(normale);
 
-    Pizza* mari = new Pizza(9, "marinara", true, 4);
-    Pizza* boschi = new Pizza(10, "boscaiola", true, 4);
-    mari->aggiungiIngredienti(*daRimuovere);
-    mari->setFarina(farro);
-    boschi->aggiungiIngredienti(*daAggiungere);
-    boschi->setFarina(normale);
+  Pizza* mari = new Pizza(9, "marinara", true, 4);
+  Pizza* boschi = new Pizza(10, "boscaiola", true, 4);
+  mari->aggiungiIngredienti(*daRimuovere);
+  mari->setFarina(farro);
+  boschi->aggiungiIngredienti(*daAggiungere);
+  boschi->setFarina(normale);
 
-    //string nome, bool disponibilita, double prezzo,
-    //unsigned int qta, double costo, QDate dataAcquisto, double capacitaBevanda)
-    Lattina* Fanta = new Lattina(11, "Fanta", true, 2, 10, 6, QDate::currentDate(), 0.33);
-    Bottiglia* CocaCola = new Bottiglia(12, "Coca Cola", true, 4, 10, 8, QDate::currentDate(), 1.5);
+  //string nome, bool disponibilita, double prezzo,
+  //unsigned int qta, double costo, QDate dataAcquisto, double capacitaBevanda)
+  Lattina* Fanta =
+      new Lattina(11, "Fanta", true, 2, 10, 6, QDate::currentDate(), 0.33);
+  Bottiglia* CocaCola =
+      new Bottiglia(12, "Coca Cola", true, 4, 10, 8, QDate::currentDate(), 1.5);
+  */
+  Comanda* comanda1 =
+      new Comanda(1, Contatto("AndyM", "Via Prova 123","123456789"),
+                  QTime(20,00), QDate(2020,06,17));
+//  comanda1->inserisciArticolo(marghe,2);
+//  comanda1->inserisciArticolo(mari, 1);
+//  comanda1->inserisciArticolo(CocaCola, 1);
 
-    bellaPadova.inserisciConsumabile(normale);
-    bellaPadova.inserisciConsumabile(farro);
-    bellaPadova.inserisciConsumabile(mozz);
-    bellaPadova.inserisciConsumabile(pomm);
-    bellaPadova.inserisciConsumabile(wrustel);
-    bellaPadova.inserisciConsumabile(ananas);
-    bellaPadova.inserisciConsumabile(funghi);
-    bellaPadova.inserisciArticolo(boschi);
-    bellaPadova.inserisciArticolo(mari);
-    bellaPadova.inserisciArticolo(marghe);
-    bellaPadova.inserisciConsumabile(Fanta);
-    bellaPadova.inserisciConsumabile(CocaCola);
+  Comanda* comanda2 =
+      new Comanda(2, Contatto("gBizz", "Via Stocazzo","987654321"),
+                  QTime(20,30), QDate(2020,06,17));
+//  comanda2->inserisciArticolo(boschi, 5);
+//  comanda2->inserisciArticolo(marghe, 1);
+//  comanda2->inserisciArticolo(Fanta, 1);
 
-    Comanda* comanda1 = new Comanda(1, Contatto("AndyM", "Via Prova 123","123456789"), QTime(20,00), QDate(2020,06,17));
-    comanda1->inserisciArticolo(marghe,2);
-    comanda1->inserisciArticolo(mari, 1);
-    comanda1->inserisciArticolo(CocaCola, 1);
-
-    Comanda* comanda2 = new Comanda(2, Contatto("gBizz", "Via Stocazzo","987654321"), QTime(20,30), QDate(2020,06,17));
-    comanda2->inserisciArticolo(boschi, 5);
-    comanda2->inserisciArticolo(marghe, 1);
-    comanda2->inserisciArticolo(Fanta, 1);
-
-    bellaPadova.inserisciComanda(comanda1);
-    bellaPadova.inserisciComanda(comanda2);
-
-
-
-    bellaPadova.eseguiComanda();
-    bellaPadova.eseguiComanda();
-
-    Risorsa* r = bellaPadova.trovaRisorsa(6);
-    bellaPadova.rimuoviConsumabile(dynamic_cast<Consumabile*>(r));
-    */
-
+  bellaPadova.inserisciComanda(comanda1);
+  bellaPadova.inserisciComanda(comanda2);
+  bellaPadova.eseguiComanda();
+//  bellaPadova.eseguiComanda();
+  view.visualizzaComande();
   return app.exec();
 }
