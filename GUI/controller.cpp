8a76f26cc85cec5f7ea64feb5e3b2771274e688d
MGufo <34510538+MGufo@users.noreply.github.com>
2020-08-17 16:46:43 +0200

    corretto bug in recuperaComande(), dove si confrontava un puntatore valido con un nullptr

--- GUI/controller.cpp
+++ GUI/controller.cpp
@@ -278,8 +278,11 @@ QList<pacchettoComanda*> *Controller::recuperaComande() const{
 
   for(auto it = comande.const_begin(); it != comande.const_end(); ++it){
     Comanda* c = *it;
-    bool eseguita = (c < current) ? true : false;
-
+    bool eseguita;
+    if(current)
+      eseguita = (c < current) ? true : false;
+    else
+      eseguita = false;
     pacchettoComanda* pC =
       new pacchettoComanda(c->getIdComanda(), c->getCliente().getNome(),
                            c->getCliente().getIndirizzo(),
