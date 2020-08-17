342fc59fc1f43b37c34f18296075c4291c7de3fe
MGufo <34510538+MGufo@users.noreply.github.com>
2020-08-17 15:54:15 +0200

    implementata funzione creaNuovaComanda()

--- GUI/controller.cpp
+++ GUI/controller.cpp
@@ -120,7 +120,19 @@ void Controller::eliminaArticolo(uint id){
 }
 
 void Controller::creaNuovaComanda(pacchettoComanda *pC){
-  // TODO: fare cose
+  Comanda* c = new Comanda();
+  c->setIdComanda(++idComande);
+  c->setCliente(Contatto(pC->nome, pC->indirizzo, pC->telefono));
+  c->setOraConsegna(pC->oraConsegna);
+  c->setDataConsegna(QDate::currentDate());
+  c->setTotale(pC->totale);
+  auto ord = pC->ordinazione;
+  for(auto it = ord.cbegin(); it!= ord.cend(); ++it){
+    c->inserisciArticolo(
+          dynamic_cast<Articolo*>(modello->trovaRisorsa((*it).first)),
+          (*it).second);
+  }
+  modello->inserisciComanda(c);
   vista->visualizzaComande();
 }
 
