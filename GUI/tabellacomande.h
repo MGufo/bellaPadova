56e7fc09ba895faba5da1817b6be2f3a40ec2e0f
killbizz <killbizz@users.noreply.github.com>
2020-08-19 18:25:46 +0200

    modificata la struttura delle tabelle: Tabella Composita è classe base astratta e viene concretizzata in TabellaRisorse e TabellaComande

--- GUI/tabellacomande.h
+++ GUI/tabellacomande.h
@@ -9,6 +9,17 @@ public:
   TabellaComande(QWidget *parent = nullptr,
                  const QString& etichetta="",
                  const QStringList* = nullptr);
+  void inserisciElemento(pacchettoComanda*);
+  virtual void rendiEditabile(bool = true) override;
+
+signals:
+  void sendComandaPacketToModel(pacchettoComanda*);
+  void sendComandaIdToModel(uint);
+
+private slots:
+ virtual void emitDataOnCellChanged(int,int) override;
+ void checkBoxToggled(bool);
+ void forwardIdToModel(uint);
 };
 
 #endif // TABELLACOMANDE_H
--- GUI/tabellarisorse.h
+++ GUI/tabellarisorse.h
@@ -1,6 +1,9 @@
 #ifndef TABELLARISORSE_H
 #define TABELLARISORSE_H
+
 #include "tabellacomposita.h"
+#include "inputValidator.h"
+#include "pushbuttonwithid.h"
 
 class TabellaRisorse : public TabellaComposita {
   Q_OBJECT
@@ -8,6 +11,22 @@ public:
   TabellaRisorse(QWidget *parent = nullptr,
                  const QString& etichetta="",
                  const QStringList* = nullptr);
+  void inserisciElemento(pacchetto*);
+  virtual void rendiEditabile(bool = true) override;
+
+signals:
+  void sendArticoloPacketToModel(pacchettoArticolo*);
+  void sendConsumabilePacketToModel(pacchettoConsumabile*);
+  void sendArticoloIdToModel(uint);
+  void sendConsumabileIdToModel(uint);
+
+private slots:
+ virtual void emitDataOnCellChanged(int,int) override;
+ void checkBoxToggled(bool);
+ void forwardIdToModel(uint);
+
+private:
+ void validateInput(int, int);
 };
 
 #endif // TABELLARISORSE_H
